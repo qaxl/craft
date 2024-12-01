84a14ac17c137940223cb193a3b6da8703b4f7be
qaxl <0xfreebuff@gmail.com>
2024-12-01 22:18:11 +0200

    initial commit
    Signed-off-by: qaxl <51490887+qaxl@users.noreply.github.com>

---
+++ src/graphics/vulkan.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <volk.h>
+
+namespace craft {
+class Instance;
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#define VK_USE_PLATFORM_XCB_KHR
+#include <volk.h>
+// TODO: in the future, use
+// [vk.xml](https://raw.githubusercontent.com/KhronosGroup/Vulkan-Docs/refs/heads/main/xml/vk.xml)
+// to load vulkan functions.
+
+int main(int argc, char **argv) {}
